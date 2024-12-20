af9904b38b8962fb7560c0655b1cb47ae30c0533
Prathamesh Bhangale <bhangaleprathamesh744@gmail.com>
2024-12-20 11:51:26 +0530

    lsolve error

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -59,22 +59,17 @@ export const auth = async (req, res, next) => {
 		}
 
 		try {
-			// Verifying the JWT using the secret key stored in environment variables
 			const decode = await jwt.verify(token, process.env.JWT_SECRET);
 			console.log(decode);
-			// Storing the decoded JWT payload in the request object for further use
 			req.user = decode;
 		} catch (error) {
-			// If JWT verification fails, return 401 Unauthorized response
 			return res
 				.status(401)
 				.json({ success: false, message: "token is invalid" });
 		}
 
-		// If JWT is valid, move on to the next middleware or request handler
 		next();
 	} catch (error) {
-		// If there is an error during the authentication process, return 401 Unauthorized response
 		return res.status(401).json({
 			success: false,
 			message: `Something Went Wrong While Validating the Token`,
